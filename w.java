92422a134bb69f1766cdd7d355c2b030088df4e0
Vaibhav Singh <you@example.com>
2025-08-13 08:52:19 +0530

    c1

--- w.java
+++ w.java
@@ -2,5 +2,10 @@ public class w {
     public static void main()
     {
         System.out.println("Vaibhav Singh");
+        System.out.println("Class 1");
+        int a=5;
+        int b=9;
+        int c=a+b;
+        System.out.println(c);
     }
 }
